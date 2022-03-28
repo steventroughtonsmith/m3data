f5cb35a412cd61dac7740612c62465187caaa072
Martin Pilkington <pilky@mcubedsw.com>
2022-03-28 14:30:17 +0100

    Initial Commit

---
+++ M3Data/M3Data.h
@@ -0,0 +1,18 @@
+//
+//  M3Data.h
+//  M3Data
+//
+//  Created by Martin Pilkington on 28/03/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for M3Data.
+FOUNDATION_EXPORT double M3DataVersionNumber;
+
+//! Project version string for M3Data.
+FOUNDATION_EXPORT const unsigned char M3DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M3Data/PublicHeader.h>
+
+
